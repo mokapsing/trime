f5e6b8ae632daaf8e666751675ba2731d548cdc8
mokapsing <39441028+mokapsing@users.noreply.github.com>
2023-09-07 14:18:59 +0800

    feat: split librime_jni

--- app/src/main/java/com/osfans/trime/core/Rime.java
+++ app/src/main/java/com/osfans/trime/core/Rime.java
@@ -254,6 +254,8 @@ public class Rime {
   private static boolean mOnMessage;
 
   static {
+    System.loadLibrary("opencc");
+    System.loadLibrary("rime");
     System.loadLibrary("rime_jni");
   }
 
