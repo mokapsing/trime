61fa7864b804567f229b55c59f5dbf7900dbb94e
mokapsing <39441028+mokapsing@users.noreply.github.com>
2023-09-08 08:24:55 +0800

    fix: timing sync not work

--- app/src/main/java/com/osfans/trime/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/osfans/trime/ui/fragments/ProfileFragment.kt
@@ -115,7 +115,7 @@ class ProfileFragment :
                     0,
                     Intent("com.osfans.trime.timing.sync"),
                     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                        PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
                     } else {
                         PendingIntent.FLAG_UPDATE_CURRENT
                     },
